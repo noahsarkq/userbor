30384015f744f5958a1ccc27da735d7cb9fa6bd6
noahsarkq <mahiraj5d@gmail.com>
2022-02-11 23:50:04 +0530

    tocu

--- ubot/plugins/sendvid.py
+++ ubot/plugins/sendvid.py
@@ -26,7 +26,8 @@ async def send_vid(bot2, vid_path, chat_ix):
             shell=True)
         thumb = f"{vid_path}.jpg"
 
-    except:
+    except Exception as e:
+        logging.info(e)
         thumb = None
     if chat_ix != 0:
         chatID = chat_ix
@@ -47,6 +48,11 @@ async def send_vid(bot2, vid_path, chat_ix):
         progress_args=(start_time1, f'Uploading {vid_path.split("/")[-1]}'))
     try:
         os.remove(vid_path)
+
+    except Exception as e:
+        logging.info(e)
+    try:
         os.remove(thumb)
+
     except Exception as e:
         logging.info(e)
