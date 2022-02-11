0870020e4b85b03351e5d3c5bb33fd9f420073c0
noahsarkq <mahiraj5d@gmail.com>
2022-02-11 10:48:31 +0530

    touchups

--- ubot/bot2.py
+++ ubot/bot2.py
@@ -20,23 +20,25 @@ async def help(_, m: Message):
 
 @bot2.on_message(filters.user(Var.chat_Id) & filters.command("stcopy"))
 async def stop_copy(_, m: Message):
-    stopCopy.append(int(m.reply_to_message.text))
+    if int(m.reply_to_message.text) == 1:
+        stopCopy.append(int(m.reply_to_message.text))
 
 
 @bot2.on_message(filters.user(Var.chat_Id) & filters.command("copy"))
 async def copy_messages(_, m: Message):
-    from_chatID, to_chatId, num1, num2 = m.reply_to_message.text.split(",")
-    for message_id in range(int(num1), int(num2) + 1):
-        if stopCopy[-1] == 1:
-            await m.reply(f"Copied Till {message_id-1}")
-            break
-        try:
-            await bot2.copy_message(chat_id=int(f"-100{to_chatId}"),
-                                    from_chat_id=int(f"-100{from_chatID}"),
-                                    message_id=message_id)
-            await asyncio.sleep(4)
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            await m.reply(f"Flood wait Occured for {e.x}")
-            pass
-    await m.reply("All Copied")
+    if "," in m.reply_to_message.text:
+        from_chatID, to_chatId, num1, num2 = m.reply_to_message.text.split(",")
+        for message_id in range(int(num1), int(num2) + 1):
+            if stopCopy[-1] == 1:
+                await m.reply(f"Copied Till {message_id-1}")
+                break
+            try:
+                await bot2.copy_message(chat_id=int(f"-100{to_chatId}"),
+                                        from_chat_id=int(f"-100{from_chatID}"),
+                                        message_id=message_id)
+                await asyncio.sleep(4)
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+                await m.reply(f"Flood wait Occured for {e.x}")
+                pass
+        await m.reply("All Copied")
