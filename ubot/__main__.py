064ddaffd26afa5e607edb2054a874767d0b2218
noahsarkq <86694875+noahsarkq@users.noreply.github.com>
2022-02-12 07:14:31 +0530

    Update __main__.py

--- ubot/__main__.py
+++ ubot/__main__.py
@@ -29,14 +29,15 @@ async def root_route_handler(request):
 async def start_services():
     print("----------------------------- DONE -----------------------------")
     print()
+    #await app.start()
+    await bot2.start()
     
     appx = web.AppRunner(await web_server())
     await appx.setup()
     bind_address = "0.0.0.0" if Var.ON_HEROKU else Var.BIND_ADDRESS
 
     await web.TCPSite(appx, bind_address, Var.PORT).start()
-    #await app.start()
-    await bot2.start()
+    
     asyncio.create_task(ping_server())
     await idle()
 
