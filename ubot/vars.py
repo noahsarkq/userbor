dd0685a7368ef27bfc35c948b208f1453aa8e2b0
noahsarkq <86694875+noahsarkq@users.noreply.github.com>
2022-02-12 06:54:48 +0530

    Update vars.py

--- ubot/vars.py
+++ ubot/vars.py
@@ -13,6 +13,7 @@ class Var(object):
     chat_Id = int(environ.get("chat_Id"))
     SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minte
     PORT = int(environ.get("PORT", 8080))
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "300"))
     BOT_TOKEN = str(environ.get("BOT_TOKEN"))
     BIND_ADDRESS = str(environ.get("WEB_SERVER_BIND_ADDRESS", "0.0.0.0"))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
